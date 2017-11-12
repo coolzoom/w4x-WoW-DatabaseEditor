834994087d86f4ca74f725db58de0da19f4a4edb
gpn39f <frankberg@foni.net>
2017-11-12 01:32:55 +0100

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WoW-DatabaseEditor
 WoW Database Editor for Database 434
+
+Editor designed for ArkCore-NG 434 Database
+
---
+++ WoW-DatabaseEditor/FormMain.vb
@@ -0,0 +1,16 @@
+﻿
+Imports ClassLibWoWDatabaseManager
+Imports ClassLibWoWCreatureItem
+
+Public Class FormMain
+    Private _databaseManager As WoWDatabaseManager
+    Private _defaultDatabaseItem As WoWDatabaseItem
+    Private _creatureManager As WoWCreatureManager
+
+    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _databaseManager = New WoWDatabaseManager
+        _creatureManager = New WoWCreatureManager
+        _defaultDatabaseItem = _databaseManager.GetDefaultWoWDatabaseItem
+
+    End Sub
+End Class
